d2c9e33e2afbbd46b436bb59e29f0c1e22a1a08f
Jean-Yves Miossec <jean-yves.miossec@auguria.fr>
2020-07-23 22:04:08 +0200

    [UPD] aaa-sale

--- aaa-addons/aaa_sale/models/sale.py
+++ aaa-addons/aaa_sale/models/sale.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from odoo import fields, models, api
 
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -10,3 +12,18 @@ class SaleOrder(models.Model):
     axe2 = fields.Many2one('crm.axes', string="Axe 2", related='opportunity_id.axe2', store=True, compute_sudo=True, readonly=True)
     axe3 = fields.Many2one('crm.axes', string="Axe 3", related='opportunity_id.axe3', store=True, compute_sudo=True, readonly=True)
     axe4 = fields.Many2one('crm.axes', string="Axe 4", related='opportunity_id.axe4', store=True, compute_sudo=True, readonly=True)
+    
+    @api.model
+    def create(self, values):
+        res = super(SaleOrder, self).create(values)
+        enjeu_id = values.get('opportunity_id')
+        stage_obj = self.env['crm.stage']
+        stage_id =  stage_obj.search([('probability', '=', 70)])
+        if stage_id:
+            query = """
+                        UPDATE crm_lead
+                        SET    stage_id = %(stage_id)s 
+                        WHERE  id = %(enjeu_id)s
+                    """
+            self._cr.execute(query, {'stage_id' : stage_id.id, 'enjeu_id' : enjeu_id})
+        return res
