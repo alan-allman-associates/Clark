55f731c98bcb04d077050185061b747763c64f77
Jean-Yves Miossec <jean-yves.miossec@auguria.fr>
2020-10-14 13:50:50 +0200

    [upd] aaa speed

--- aaa-addons/aaa_speed/models/res_company.py
+++ aaa-addons/aaa_speed/models/res_company.py
@@ -8,17 +8,3 @@ class ResCompany(models.Model):
 
     manage_speed = fields.Boolean(string="Manage Speed", default=True, store=True)
  
-    @api.onchange('manage_speed')
-    def _onchange_manage_speed(self):
-		group = self.env.ref('aaa_speed.group_manage_speed')
-        users = self.env['res.users'].search([('company_id', '=', self.id)])
-		if manage_speed:
-		   for user in users:
-		       group.write({
-			   [(4, user.id)]
-			   })
-		else:
-		   for user in users:
-		       group.write({
-			   [(3, user.id)]
-			   })
