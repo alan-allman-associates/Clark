37a5e5a83a4ff65039ea911dec8cd46e3707d828
Abdessamad HILALI <abdessamad.hilali@auguria.fr>
2021-01-25 17:10:57 +0100

    [IMP] crm

--- aaa-addons/aaa_crm/models/crm_lead.py
+++ aaa-addons/aaa_crm/models/crm_lead.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo import api, SUPERUSER_ID
 
 SELECTION_AXES = [('axe1', 'Axe 1'), ('axe2', 'Axe 2'), ('axe3', 'Axe 3'), ('axe4', 'Axe 4'), ('axe5', 'Sous domaine')]
 
@@ -22,4 +23,14 @@ class CrmLead(models.Model):
     axe5 = fields.Many2one('crm.axes', string="Sous domaine", domain="[('axe_type', '=', 'axe5')]")
 
     end_lead = fields.Datetime(string='Answer date limit')
- 
+    user_is_subdomain = fields.Boolean('IS subdomain user', compute='compute_is_subdomain')
+
+    @api.depends('axe5')
+    def compute_is_subdomain(self):
+        current_user = self.env.user
+        group = 'aaa_crm.axe5_sous_domain'
+        for order in self:
+            if current_user.id == SUPERUSER_ID:
+                order.user_is_subdomain = True
+            else:
+                order.user_is_subdomain = current_user.has_group(group)
