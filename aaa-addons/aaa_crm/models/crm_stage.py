1180b4e25c2eb3d525baa4aacf844b79826a9e8e
houriatalouzet <62991465+houriatalouzet@users.noreply.github.com>
2021-04-07 11:54:18 +0200

    Update crm_stage.py

--- aaa-addons/aaa_crm/models/crm_stage.py
+++ aaa-addons/aaa_crm/models/crm_stage.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 
 
 
-class CrmLead(models.Model):
-    _inherit = "crm.lead"
+class CrmStage(models.Model):
+    _inherit = "crm.stage"
 
     is_proposal = fields.Boolean(string="Est une etape de proposition")
