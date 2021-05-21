b7866f8a8d22ef8fbc9b0b484df85c44e6f2018d
Abdessamad HILALI <abdessamad.hilali@auguria.fr>
2021-05-21 11:54:30 +0200

    [IMP] cron partner

--- aaa-addons/aaa_contact/models/res_partner.py
+++ aaa-addons/aaa_contact/models/res_partner.py
@@ -11,38 +11,14 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    def log(self, message, level="info"):
-        with self.pool.cursor() as cr:
-            cr.execute("""
-                INSERT INTO ir_logging(create_date, create_uid, type, dbname, name, level, message, path, line, func)
-                VALUES (NOW() at time zone 'UTC', %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, (self.env.uid, 'server', self._cr.dbname, __name__, level, message, "action", self.id, self.name))
-
     def partner_deactivate(self):
-        partners_deactivate = self.env['res.partner']
         _logger.info('Scheduler is running to deactivate partner with name with @')
         nowDatetime = datetime.now()
-        partners = self.env['res.partner'].search(
-            ['&',
-             '&',
-             '&',
-             ('email', '!=', 'admin'),
-             ('is_company', '=', False),
-             ('active', '=', True),
-             '|',
-             ('parent_id', '=', False),
-             ('parent_id.active', '=', False),
-             '|',
-             ('name', 'not like', '[%'),
-             '|',
-             ('name', 'not like', '%]%'),
-             ('name', 'like', '%@%')])
-        partners and partners[0].log("Nbre de contact est %s" %(len(partners)))
-        partners and partners[0].log("Les contacts sont %s" %(partners.mapped('name')))
+        partners = self.env['res.partner'].search([('name','like','%@%'),('active','=', True)])
         for partner in partners:
             deactive = True
             attendees = self.env['calendar.attendee'].search([('partner_id','=', partner.id)])
-            if attendees: 
+            if attendees:
                 for attendee in attendees:
                     events = self.env['calendar.event'].search([('id','=', attendee.event_id.id)])
                     if events:
@@ -50,16 +26,6 @@ class ResPartner(models.Model):
                             if event.start > nowDatetime:
                                 deactive = False
                 if deactive:
-                    partners_deactivate += partner
+                    partner.active = False
                     _logger.info('Contact desactive: {}'.format(partner.name))
-        template = self.env.ref('aaa_contact.partner_active_false', raise_if_not_found=False)
-        if template and partners_deactivate:
-            partner_to = self.env['ir.config_parameter'].sudo().get_param('partner_to.send_deactivate_partner')
-            email_from = self.env['ir.config_parameter'].sudo().get_param('email_from.send_deactivate_partner')
-            partners_deactivate.write({'active': False})
-            template.with_context(partner_to=int(partner_to),
-                                  email_from = email_from,
-                                  partner_ids = partners_deactivate.ids).send_mail(self.id,
-                                                       email_values={'subject' : "La liste des contacts archivés", })
         _logger.info('Contact Scheduler: Successfully deactivate partner')
-
