e386ec31078d94b088f62a328363cbf0216cca9d
ahilali-odoo <72794805+ahilali-odoo@users.noreply.github.com>
2021-06-21 21:00:46 +0200

    Update res_partner.py

--- aaa-addons/aaa_contact/models/res_partner.py
+++ aaa-addons/aaa_contact/models/res_partner.py
@@ -38,7 +38,7 @@ class ResPartner(models.Model):
              ('name', 'not like', '[%'),
              '|',
              ('name', 'not like', '%]%'),
-             ('name', 'like', '%@%')], limit=100)
+             ('name', 'like', '%@%')], limit=20)
         partners and partners[0].log("Nbre de contact est %s" %(len(partners)))
         partners and partners[0].log("Les contacts sont %s" %(partners.mapped('name')))
         for partner in partners:
@@ -55,13 +55,11 @@ class ResPartner(models.Model):
                     partners_deactivate += partner
                     _logger.info('Contact desactive: {}'.format(partner.name))
         template = self.env.ref('aaa_contact.partner_active_false', raise_if_not_found=False)
-        if template and partners_deactivate:
+        if partners_deactivate:
             partner_to = self.env['ir.config_parameter'].sudo().get_param('partner_to.send_deactivate_partner')
             email_from = self.env['ir.config_parameter'].sudo().get_param('email_from.send_deactivate_partner')
             partners_deactivate.write({'active': False, 'archive_script':True})
-            template.with_context(partner_to=int(partner_to),
-                                  email_from = email_from,
-                                  partner_ids = partners_deactivate.ids).send_mail(self.id,
-                                                       email_values={'subject' : "La liste des contacts archivés", })
-        _logger.info('Contact Scheduler: Successfully deactivate partner')
+            if template and partner_to and email_from:
+                template.with_context(partner_to=int(partner_to), email_from = email_from, partner_ids = partners_deactivate.ids).send_mail(self.id, email_values={'subject' : "La liste des contacts archivés", })
+                _logger.info('Contact Scheduler: Successfully deactivate partner')
 
